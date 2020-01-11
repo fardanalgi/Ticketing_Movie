183f87cad1957b49938ebd2e33e3bdf1ff6e7751
fardanalgi <fardanalgi@gmail.com>
2020-01-11 22:24:50 +0700

    commit all

---
+++ app/src/main/java/com/fardancompany/mov/onboarding/OnboardingSatuActivity.kt
@@ -0,0 +1,13 @@
+package com.fardancompany.mov.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fardancompany.mov.R
+
+class OnboardingSatuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_satu)
+    }
+}
