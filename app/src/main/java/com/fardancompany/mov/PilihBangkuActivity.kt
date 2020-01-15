c1521dcb87ba74e28f2da588ebdeb336049ecaec
fardanalgi <fardanalgi@gmail.com>
2020-01-15 08:59:08 +0700

    commit all

---
+++ app/src/main/java/com/fardancompany/mov/PilihBangkuActivity.kt
@@ -0,0 +1,12 @@
+package com.fardancompany.mov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PilihBangkuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilih_bangku)
+    }
+}
