69651ab6e6b89f1af28b86a6f1c1782149a1762d
fardanalgi <fardanalgi@gmail.com>
2020-01-19 23:46:43 +0700

    commit all

--- app/src/main/java/com/fardancompany/mov/onboarding/OnboardingTigaActivity.kt
+++ app/src/main/java/com/fardancompany/mov/onboarding/OnboardingTigaActivity.kt
@@ -1,13 +1,24 @@
 package com.fardancompany.mov.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fardancompany.mov.R
+import com.fardancompany.mov.sign.SignInActivity
+import kotlinx.android.synthetic.main.activity_onboarding_tiga.*
 
 class OnboardingTigaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_tiga)
+
+        btn_home.setOnClickListener {
+            finishAffinity()
+
+            val intent = Intent (this@OnboardingTigaActivity,
+                SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
