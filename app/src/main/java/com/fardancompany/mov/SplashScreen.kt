ffd1324dfe23229ad22d25735d6bddb312abe8aa
fardanalgi <fardanalgi@gmail.com>
2020-01-19 22:27:16 +0700

    add splashscreen

--- app/src/main/java/com/fardancompany/mov/SplashScreen.kt
+++ app/src/main/java/com/fardancompany/mov/SplashScreen.kt
@@ -1,12 +1,23 @@
 package com.fardancompany.mov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.fardancompany.mov.onboarding.OnboardingSatuActivity
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splashscreen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreen,
+                OnboardingSatuActivity::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
     }
 }
