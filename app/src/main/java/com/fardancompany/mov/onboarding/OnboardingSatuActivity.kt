774e6b3ab25cfd581ca01f35dc569d42b3e20817
fardanalgi <fardanalgi@gmail.com>
2020-01-19 23:32:38 +0700

    add on boarding

--- app/src/main/java/com/fardancompany/mov/onboarding/OnboardingSatuActivity.kt
+++ app/src/main/java/com/fardancompany/mov/onboarding/OnboardingSatuActivity.kt
@@ -1,13 +1,30 @@
 package com.fardancompany.mov.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fardancompany.mov.R
+import com.fardancompany.mov.sign.SignInActivity
+import kotlinx.android.synthetic.main.activity_onboarding_satu.*
 
 class OnboardingSatuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_satu)
+
+        btn_home.setOnClickListener {
+            val intent = Intent(this@OnboardingSatuActivity,
+                OnboardingduaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_daftar.setOnClickListener {
+            finishAffinity()
+
+            val intent = Intent(this@OnboardingSatuActivity,
+                SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
