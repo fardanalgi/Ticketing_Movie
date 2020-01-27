88a45f062c8c794b644608e95cb456c6789ee554
fardanalgi <fardanalgi@gmail.com>
2020-01-27 22:42:11 +0700

    commit

--- app/src/main/java/com/fardancompany/mov/sign/signup/SignUpPhotoscreenActivity.kt
+++ app/src/main/java/com/fardancompany/mov/sign/signup/SignUpPhotoscreenActivity.kt
@@ -1,10 +1,15 @@
 package com.fardancompany.mov.sign.signup
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fardancompany.mov.R
 
 class SignUpPhotoscreenActivity : AppCompatActivity() {
+
+    val REQUEST_TIME_CAPTURE = 1
+    var statusAdd: Boolean = false
+    lateinit var filePath : Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
