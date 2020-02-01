e682c92485cccc301ca2a74e100561bd27abc4eb
fardanalgi <fardanalgi@gmail.com>
2020-02-01 22:25:31 +0700

    add poto screen

--- app/src/main/java/com/fardancompany/mov/sign/signup/SignUpPhotoscreenActivity.kt
+++ app/src/main/java/com/fardancompany/mov/sign/signup/SignUpPhotoscreenActivity.kt
@@ -1,18 +1,153 @@
 package com.fardancompany.mov.sign.signup
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.fardancompany.mov.HomeActivity
 import com.fardancompany.mov.R
+import com.fardancompany.mov.utils.Preferences
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import kotlinx.android.synthetic.main.activity_sign_up_photoscreen.*
+import java.util.*
 
-class SignUpPhotoscreenActivity : AppCompatActivity() {
 
-    val REQUEST_TIME_CAPTURE = 1
+class SignUpPhotoscreenActivity : AppCompatActivity(), PermissionListener {
+
+    val REQUEST_IMAGE_CAPTURE = 1
     var statusAdd: Boolean = false
     lateinit var filePath : Uri
+
+    lateinit var storage: FirebaseStorage
+    lateinit var storageReference: StorageReference
+    lateinit var preferences: Preferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_photoscreen)
+
+        preferences = Preferences(this)
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage.getReference()
+
+        tv_helllo.text = "Selamat datang\n"+intent.getStringExtra("nama")
+
+        iv_add.setOnClickListener{
+            if (statusAdd){
+                statusAdd = false
+                btn_home.visibility = View.INVISIBLE
+                iv_add.setImageResource(R.drawable.ic_btn_upload)
+                iv_profile.setImageResource(R.drawable.pic)
+            }else{
+                Dexter.withActivity(this)
+                    .withPermission(Manifest.permission.CAMERA)
+                    .withListener(this)
+                    .check()
+            }
+        }
+
+        btn_skip.setOnClickListener {
+
+            finishAffinity()
+
+            val intent = Intent(this@SignUpPhotoscreenActivity,
+                HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_home.setOnClickListener{
+            if (filePath != null){
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Uploading...")
+                progressDialog.show()
+
+                val ref = storageReference.child("image/" + UUID.randomUUID().toString())
+                ref.putFile(filePath)
+                    .addOnSuccessListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(this@SignUpPhotoscreenActivity,"Upload", Toast.LENGTH_SHORT).show()
+
+                        ref.downloadUrl.addOnSuccessListener {
+                            preferences.setValues("url", it.toString())
+                        }
+
+                        finishAffinity()
+                        val intent = Intent(this@SignUpPhotoscreenActivity,
+                            HomeActivity::class.java)
+                        startActivity(intent)
+                    }
+                    .addOnFailureListener { e ->
+                        progressDialog.dismiss()
+                        Toast.makeText(this@SignUpPhotoscreenActivity,"Failed" + e.message, Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnProgressListener { taskSnapshot ->
+                        val progress = 100.0 * taskSnapshot.bytesTransferred / taskSnapshot
+                            .totalByteCount
+                        progressDialog.setMessage("Upload" + progress.toInt()+"%")
+                    }
+
+            }
+        }
+
+    }
+
+    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onPermissionRationaleShouldBeShown(
+        permission: com.karumi.dexter.listener.PermissionRequest?,
+        token: PermissionToken?
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, "Anda tidak bisa menambahkan photo profile", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this,"Tergesah? klik tombol Upload nanti", Toast.LENGTH_LONG).show()
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+            var bitmap = data?.extras?.get("data") as Bitmap
+            statusAdd = true
+
+            filePath = data.getData()!!
+            Glide.with(this)
+                .load(bitmap)
+                .apply(RequestOptions.circleCropTransform())
+                .into(iv_profile)
+
+            btn_home.visibility = View.VISIBLE
+            iv_add.setImageResource(R.drawable.ic_btn_del_pic)
+        }
     }
 }
